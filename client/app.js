318401fdf8bef47f40d111e6749ef68bc3afb804
Rafał Pietrzak <rpietrzak@pixelage.pl>
2020-06-17 16:58:39 +0200

    Add functionality to client

--- client/app.js
+++ client/app.js
@@ -4,11 +4,37 @@ const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
-const login = () => {
-  console.log(userNameInput.nodeValue);
+let userName = null;
+const login = (event) => {
+  event.preventDefault();
+  if(userNameInput.value){
+    userName = userNameInput.value;
+    messagesSection.classList.add('show');
+  } else {
+    alert('Add name to field');
+  }
 }; 
-
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault;
-  login();
-});
+const sendMessage = (event) => {
+  event.preventDefault();
+  if(messageContentInput.value) {
+    addMessage(userName, messageContentInput.value)
+    messageContentInput.value = null;
+  } else {
+    alert('Before click button add same message!')
+  }
+}
+const addMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage)
