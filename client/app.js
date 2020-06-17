cd0719bee516740f849788caa99866805383abcc
Rafał Pietrzak <rpietrzak@pixelage.pl>
2020-06-17 15:50:21 +0200

    set server

---
+++ client/app.js
@@ -0,0 +1,14 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+const login = () => {
+  console.log(userNameInput.nodeValue);
+}; 
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault;
+  login();
+});
